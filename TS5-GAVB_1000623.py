680caaea2b78a560a52363b1faca33cdb1830297
Alejandro-Valle <143530888+Alejandro-Valle@users.noreply.github.com>
2023-09-07 07:42:08 -0600

    TS5-GAVB_1000623

---
+++ TS5-GAVB_1000623.py
@@ -0,0 +1,20 @@
+"""
+Introducción a la programación Sección 18
+7/09/2023
+Alejandro Valle
+"""
+sNombre = input("Ingrese su nombre: ") 
+sEdad = input("Ingrese su edad: ")
+sCarrera = input("ingrese su carrera: ")
+sCarne = input("Ingrese su carne: ")
+
+print("Mi segundo programa")
+print("Nombre: "+ sNombre)
+print("Edad: "+ sEdad)
+print("Carrera: "+ sCarrera)
+print("Carné: "+ sCarne)
+
+print("Soy " + sNombre, end="")
+print(" tengo " + sEdad, end="")
+print(" años y estudio la carrera de " + sCarrera)
+print("Mi número de carné es " + sCarne, end="")
