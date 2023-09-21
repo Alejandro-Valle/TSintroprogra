7c2cdeca4008ebf6cea9fbc2a131fc3ca83a7254
Alejandro-Valle <143530888+Alejandro-Valle@users.noreply.github.com>
2023-09-21 08:14:55 -0600

    Add files via upload

---
+++ TS7-GAVB_1000623.py
@@ -0,0 +1,41 @@
+# TS Intro a la programación
+# 21/09/2023
+#Alejandro Valle 1000623
+
+print("Selecciones un número del 1 al 10")
+x=input()
+x=int(x)
+    
+if(x==10):
+    print("La tabla de multiplicar de 10 es")
+    print(x*1, x*2, x*3, x*4, x*5, x*6, x*7, x*8, x*9, x*10)
+if(x==9):
+    print("La tabla de multiplicar de 9 es")
+    print(x*1, x*2, x*3, x*4, x*5, x*6, x*7, x*8, x*9, x*10)
+if(x==8):
+    print("La tabla de multiplicar de 8 es")
+    print(x*1, x*2, x*3, x*4, x*5, x*6, x*7, x*8, x*9, x*10)
+if(x==7):
+    print("La tabla de multiplicar de 7 es")
+    print(x*1, x*2, x*3, x*4, x*5, x*6, x*7, x*8, x*9, x*10)
+if(x==6):
+    print("La tabla de multiplicar de 6 es")
+    print(x*1, x*2, x*3, x*4, x*5, x*6, x*7, x*8, x*9, x*10)
+if(x==5):
+    print("La tabla de multiplicar de 5 es")
+    print(x*1, x*2, x*3, x*4, x*5, x*6, x*7, x*8, x*9, x*10)
+if(x==4):
+    print("La tabla de multiplicar de 4 es")
+    print(x*1, x*2, x*3, x*4, x*5, x*6, x*7, x*8, x*9, x*10)
+if(x==3):
+    print("La tabla de multiplicar de 3 es")
+    print(x*1, x*2, x*3, x*4, x*5, x*6, x*7, x*8, x*9, x*10)
+if(x==2):
+    print("La tabla de multiplicar de 2 es")
+    print(x*1, x*2, x*3, x*4, x*5, x*6, x*7, x*8, x*9, x*10)
+if(x==1):
+    print("La tabla de multiplicar de 1 es")
+    print(x*1, x*2, x*3, x*4, x*5, x*6, x*7, x*8, x*9, x*10)
+    
+    
+    
